9fd0b3d73b433915f181f1920b9002c93ab852e5
Userded-19 <abinav10102@gmail.com>
2023-09-21 13:49:07 +0530

    update

--- search/DSA.java
+++ search/DSA.java
@@ -42,15 +42,3 @@ import java.util.*;
 // }
 
 // }
-
-/**
- * DSA
- */
-public class DSA {
-
-    public static void main(String[] args) {
-
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-    }
-}
